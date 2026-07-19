4d50f8ecbd941a4107f13944dd8273bfc7686f9d
Tapia Maldonado Eduardo Agustin <43558598agustin@gmail.com>
2026-07-19 03:05:14 -0300

    Agrega configuracion PWA con manifest, service worker e icono para instalacion nativa

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'handball-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
